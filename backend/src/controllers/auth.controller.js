1ef8e02b66a9269eef33cdf91d5a053f75746437
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-04-16 11:01:08 -0300

    backend + db setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup =  (req, resp) =>{
+    resp.send('signup route')
+}
+
+export const login =  (req, resp) =>{
+    resp.send('login route')
+}
+
+export const logout =  (req, resp) =>{
+    resp.send('logout route')
+}
